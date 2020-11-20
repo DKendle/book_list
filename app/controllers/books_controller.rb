7cbb80a2f08977ddb05b56583b9e0d094fddf43d
DKendle <dina.kendle@gmail.com>
2020-11-20 17:17:10 -0500

    Adds new book form with fields_for review areas

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,46 @@
 class BooksController < ApplicationController
+
+    def index
+    end
+
+    def show
+        @book
+    end
+
+    def new
+        @book = Book.new
+        @book.reviews.build
+    end
+
+    def create
+        book = Book.new(book_params)
+        if book.valid?
+            book.save
+            redirect_to book_path(book)
+        else 
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permits(:title, :author, :read, :currently_own, 
+            reviews_attributes: [
+                :title,
+                :description,
+                :rating,
+                :user_id
+            ]
+        )
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
-    
+    accepts_nested_attributes_for :reviews
 end
