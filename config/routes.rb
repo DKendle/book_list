88c3627b05ddae16b773b6899fc8bcd4cc1bbac7
DKendle <dina.kendle@gmail.com>
2020-11-20 15:50:09 -0500

    Nests review routes under books

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :books
   root "welcome#home"
+  
+  resources :books do
+    resources :reviews, only: [:show, :index, :new]
+  end
+
+  ####Review CRUD functions that are not nested are deactivated.
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
