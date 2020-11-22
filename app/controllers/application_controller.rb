0f05859ffd8c6058c9e25ff236baf175a8416059
DKendle <dina.kendle@gmail.com>
2020-11-22 11:07:22 -0500

    Creates link to new user from homepage, adds session id assignment to created user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
 
     def current_user?
-        #Check current session[:id] 
+        #Check current session[:id] matches 
+        session[:id] = @user.id
     end
 
     def logged_in?
         #Checks whether user is logged in or not
-
+        if !session[:id].nil?
+        end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,18 @@ class UsersController < ApplicationController
 
     def new
         @user = User.new
-        @user.books.build
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            session[:id] = @user.id
+            redirect_to user_path(@user)
+            byebug
+        else 
+            render :new 
+        end
     end
 
     def edit
@@ -21,13 +29,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, 
-            books_attributes: [
-                :title,
-                :author,
-                :read,
-                :currently_own
-            ]
-        )
+        params.require(:user).permit(:name, :email, :password_digest)
     end
 end
