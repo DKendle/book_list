39cad478618794d140952e61719a0209df9736a8
DKendle <dina.kendle@gmail.com>
2020-12-14 15:48:45 -0500

    Adds scope method to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
     has_many :users, through: :reviews
     accepts_nested_attributes_for :reviews
     validates_presence_of :title
-    
+    scope :not_read , { where(read: "false" ) }
 end
